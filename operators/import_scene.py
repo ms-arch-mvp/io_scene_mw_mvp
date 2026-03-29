57105a14a9b9062e4f650922b4e9d6cde26207cb
MS <79557881+ms-arch-mvp@users.noreply.github.com>
2026-03-29 13:48:59 +1100

    Add files via upload

--- operators/import_scene.py
+++ operators/import_scene.py
@@ -60,6 +60,7 @@ class ImportScene(bpy.types.Operator, ImportHelper):
     ignore_billboards: bpy.props.BoolProperty(default=False, options={"HIDDEN"})
     ignore_shadow_meshes: bpy.props.BoolProperty(default=False, options={"HIDDEN"})
     ignore_switch_names: bpy.props.StringProperty(default="", options={"HIDDEN"})
+    filter_best_lod: bpy.props.BoolProperty(default=False, options={"HIDDEN"})
     always_use_file_name_for_root_name: bpy.props.BoolProperty(default=False, options={"HIDDEN"})
     use_texture_path_in_material_name: bpy.props.BoolProperty(default=False, options={"HIDDEN"})
 
